f702dd5a0f518d0bf4ab4d6cf5e0d5ffc5075242
Andrew McCann <amccann@ualberta.ca>
2012-08-15 11:57:07 -0600

    Made insignificant changes required for release.

--- version.php
+++ version.php
@@ -1,36 +1,11 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-
-/**
- * Defines the version of vidtrans
- *
- * This code fragment is called by moodle_needs_upgrading() and
- * /admin/index.php
- *
- * @package    mod
- * @subpackage vidtrans
- * @copyright  2011 Your Name
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2012022913;      // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2010031900;      // Requires this Moodle version
-$module->cron      = 1;               // Period for cron to check this module (secs)
-$module->component = 'mod_vidtrans'; // To check on upgrade, that module sits in correct place
+$plugin->version = 2012081500;
+$plugin->requires = 2010031900;
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '0.9.0';
+$module->component = 'mod_vidtrans';
+$module->cron = 1;
+
