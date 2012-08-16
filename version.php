c867c40097877aedeba8fe37d72ff2b7cae57c47
Andrew McCann <amccann@ualberta.ca>
2012-08-16 08:40:52 -0600

    Fixed Issue #1 Fixed Issue #2

--- version.php
+++ version.php
@@ -2,10 +2,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2012081500;
-$plugin->requires = 2010031900;
-$plugin->maturity = MATURITY_RC;
-$plugin->release = '0.9.0';
+$module->version = 2012081500;
+$module->requires = 2010031900;
+$module->maturity = MATURITY_RC;
+$module->release = '0.9.0';
 $module->component = 'mod_vidtrans';
 $module->cron = 1;
 
